f2e377d434e21ab8373c4707a0b5735fb9b40886
Nikita <nickita.kozeev25@gmail.com>
2026-09-07 09:53:17 +0300

    Delete AP1_Py_T03_ID_1375365-1 directory

--- AP1_Py_T03_ID_1375365-1/src/domain/service/game_service.py
+++
@@ -1,9 +0,0 @@
-class GameService:
-    def get_next_move(self, game):
-        raise NotImplementedError
-    
-    def validate_board(self, game, new_board_matrix):
-        raise NotImplementedError
-    
-    def check_game_over(self, board):
-        raise NotImplementedError
